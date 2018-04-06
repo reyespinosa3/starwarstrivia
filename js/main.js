0b543324d03899e31757ffa18bb4574ac874fddd
Rey Espinosa <reynaldo.espinosa.III@gmail.com>
2018-04-06 12:26:36 -0500

    finalized css style, has responsive design now, buttons disappear on small window or mobile device view

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 console.log("Up and Running");
+
+// create object array with answers
+// create logic to display image & answers in buttons
+// create logic to update progess bar
+
+let questions = {
+  "ans":"Princess Leia",
+  "alt":"Queen Amidala",
+  "alt1":"Your Mom"
+},
+{
+  "ans": "Han Solo",
+  "alt": "Boba Fett",
+  "alt1": "Luke Skwalker"
+},
+{
+  "ans": "Chewbacca",
+  "alt": "Ewok",
+  "alt1": "Jabba Hutt"
+},
+{
+  "ans": "Jabba Hutt",
+  "alt": "The Blob",
+  "alt1": "Slimer"
+},
+{
+  "ans": "R2-D2",
+  "alt": "C3-PO",
+  "alt1": "BB-12"
+};
