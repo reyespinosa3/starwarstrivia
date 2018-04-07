77fdd2093b4184fb4d535e26a6f9810687bc1e5b
Rey Espinosa <reynaldo.espinosa.III@gmail.com>
2018-04-07 14:00:48 -0500

    had to recode due to issue with button click, now work fine, next work on progress bar and images

--- js/main.js
+++ js/main.js
@@ -5,12 +5,27 @@ console.log("Up and Running");
 // create logic to update progess bar
 // create logic for next question button
 
-let questions = [
-  {"corans":"Princess Leia", "ans":"Queen Amidala", "ans1":"Princess Leia", "ans2":"Your Mom"},
-  {"corans":"Han Solo", "ans":"Han Solo", "ans1":"Boba Fett", "ans2":"Luke Skywalker"},
-  {"corans":"Chewbacca", "ans":"Chewbacca", "ans1":"Ewok", "ans2":"Jabba Hutt"},
-  {"corans":"Jabba Hutt", "ans":"Jabba Hutt", "ans1":"The Blob", "ans2":"Slimer"},
-  {"corans":"R2-D2", "ans":"R2-D2", "ans1":"C3-PO", "ans2":"BB-12"}
+const questions = [
+  {"ans":"Queen Amidala", "ans1":"Princess Leia", "ans2":"Your Mom"},
+  {"ans":"Han Solo", "ans1":"Boba Fett", "ans2":"Luke Skywalker"},
+  {"ans":"Chewbacca", "ans1":"Ewok", "ans2":"Jabba Hutt"},
+  {"ans":"Jabba Hutt", "ans1":"The Blob", "ans2":"Slimer"},
+  {"ans":"Teddy Bear", "ans1":"Wookie", "ans2":"Ewok"},
+  {"ans":"Darth Maul", "ans1":"Darth Vader", "ans2":"Darth Sidious"},
+  // {"ans":"R2-D2", "ans1":"C3-PO", "ans2":"BB-12"}
+];
+
+const answers = [
+  "Princess Leia",
+  "Han Solo",
+  "Chewbacca",
+  "Jabba Hutt",
+  "Ewok",
+  "Darth Maul",
+  "R2-D2",
+  "Jawa",
+  "Darth Vader",
+  "Luke Skywalker"
 ];
 
 const charPics = [
@@ -18,50 +33,87 @@ const charPics = [
   "images/characters/hansolo.png",
   "images/characters/chewbacca.png",
   "images/characters/jabbahutt.png",
-  "images/characters/r2d2.png"
-]
+  "images/characters/ewok.png",
+  "images/characters/darthmaul.png",
+  "images/characters/r2d2.png",
+  "images/characters/jawa.png",
+  "images/characters/darthvader.png",
+  "images/characters/lukeskywalker.png"
+];
 
 let i=0;
+let count=4;
+let aOne = $('#answer1');
+let aTwo = $('#answer2');
+let aThree = $('#answer3');
+let correct = 0;
 
-const loadQuestions = function() {
+const loadButtons = function() {
   $('#starwarscharacter').attr("src", charPics[i]);
-  $('#answer1').text(questions[i].ans);
-  $('#answer2').text(questions[i].ans1);
-  $('#answer3').text(questions[i].ans2);
-  $('.answer').on('click', function(e) {
-      e.preventDefault();
-      if (event.target.id === "answer1") {
-        if ($('#answer1').text() === questions[i].corans) {
-          alert("You got it right!");
-        } else {
-          alert("Sorry, please click Next button.")
-        }
-      } else if (event.target.id === "answer2") {
-          console.log("answer 2 button clicked");
-          if ($('#answer2').text() === questions[i].corans) {
-            alert("You got it right!");
-          } else {
-            alert("Sorry, please click Next button.")
-          }
-        } else if (event.target.id === "answer3") {
-          console.log("answer 3 button clicked");
-          if ($('#answer3').text() === questions[i].corans) {
-            alert("You got it right!");
-          } else {
-            alert("Sorry, please click Next button.")
-          }
-        } else if (event.target.id === "nextQuestion") {
-          i += 1;
-          console.log("next question button clicked");
-          loadQuestions();
-        }
-      });
-  };
+  $(aOne).removeClass("hidden");
+  $(aTwo).removeClass("hidden");
+  $(aThree).removeClass("hidden");
+  checkAnswers();
+}
+
+const updateButtons = function() {
+  $(aOne).addClass("hidden");
+  $(aTwo).addClass("hidden");
+  $(aThree).addClass("hidden");
+  let a = "#answer" + count;
+  aOne = $(a);
+  let b = "#answer" + (count + 1);
+  aTwo = $(b);
+  let c = "#answer" + (count + 2);
+  aThree = $(c);
+  count +=3;
+  loadButtons();
+}
+
+const checkAnswers = function() {
+  $(aOne).on('click', function(e) {
+    if (aOne.text() === answers[i]) {
+      console.log("Correct");
+      i += 1;
+      correct++;
+      updateButtons();
+    } else {
+      console.log("Wrong");
+      i += 1;
+      updateButtons();
+    }
+  });
+
+  $(aTwo).on('click', function(e) {
+    if (aTwo.text() === answers[i]) {
+      console.log("Correct");
+      i += 1;
+      correct++;
+      updateButtons();
+    } else {
+      console.log("Nope");
+      i += 1;
+      updateButtons();
+    }
+  });
+
+  $(aThree).on('click', function(e) {
+    if (aThree.text() === answers[i]) {
+      console.log("Correct");
+      i += 1;
+      correct++;
+      updateButtons();
+    } else {
+      console.log("Sorry Charlie");
+      i += 1;
+      updateButtons();
+    }
+  });
+}
 
 $('#startGame').on('click', function(e) {
-  e.preventDefault();
   console.log("Game Starts");
   $('#trainingJedi').hide();
   $('#masterJedi').hide();
-  loadQuestions();
-  });
+  loadButtons();
+  })
