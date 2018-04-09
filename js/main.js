4dfe07b5a7562b7727501896d6e96f7f012454a7
Rey Espinosa <reynaldo.espinosa.III@gmail.com>
2018-04-09 15:34:21 -0500

    added comments in js to show what each function does

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ console.log("Up and Running");
 // create logic to update progess bar
 // create logic for next question button
 
+// array of correct answers
 const answers = [
   "Princess Leia",
   "Han Solo",
@@ -17,9 +18,10 @@ const answers = [
   "Darth Vader",
   "Luke Skywalker",
   "Boba Fett",
-  "C-3PO"
+  "C-3P0"
 ];
 
+// array of character pictures
 const charPics = [
   "images/characters/princessleia.png",
   "images/characters/hansolo.png",
@@ -35,6 +37,7 @@ const charPics = [
   "images/characters/c3po.png"
 ];
 
+// global variables
 let i=0;
 let count=4;
 let aOne = $('#answer1');
@@ -43,6 +46,7 @@ let aThree = $('#answer3');
 let correct = 0;
 let level = 0;
 
+// code to execute at end of the game
 const endGame = function() {
     $('#starwarscharacter').addClass("hidden");
     $('#yodadance').addClass("hidden");
@@ -54,6 +58,7 @@ const endGame = function() {
     $(aThree).addClass("hidden");
 }
 
+// code to execute to show current answer buttons
 const loadButtons = function() {
   $('#starwarscharacter').attr("src", charPics[i]);
   $(aOne).removeClass("hidden");
@@ -62,18 +67,19 @@ const loadButtons = function() {
   checkProgress();
 }
 
+// code to update progress bar and images that coinside with progress
 const checkProgress = function() {
   if (correct == 10) {
     $('#currentLevel').attr("style", "color:yellow");
     $('#masterJedi').show();
     $('#myBar').attr("style", "width:100%");
-    $('#level').text("Jedi Master");
+    $('#level').text("jedi master");
     new Audio("audio/jedi-know.mp3").play();
     endGame();
   } else if (correct == 5) {
     $('#currentLevel').attr("style", "color:blue");
     $('#trainingJedi').show();
-    $('#level').text("Jedi In Training");
+    $('#level').text("jedi In training");
     new Audio("audio/forcestrong.mp3").play();
   } else {
     let prog = level + "%";
@@ -82,6 +88,8 @@ const checkProgress = function() {
   checkEndGame();
 }
 
+// code to check if game is over due to all questions being asked
+// but user not getting ten correct
 const checkEndGame = function() {
 console.log(count);
   if (count === 40) {
@@ -91,6 +99,8 @@ console.log(count);
   }
 }
 
+// code to update buttons to show next set of answers for new
+// character picture
 const updateButtons = function() {
   $(aOne).addClass("hidden");
   $(aTwo).addClass("hidden");
@@ -105,11 +115,16 @@ const updateButtons = function() {
   loadButtons();
 }
 
+// code used to update correct answer on front page so user can
+// see the correct answer to all the questions
 const nextCharacter = function() {
   $('#contTraining').text("Correct Answer was " + answers[i-1]);
   updateButtons();
 }
 
+// code used to compare user selection to correct answer, also
+// updates progress bar, nubmer of correct answers and correct
+// answer array
 const checkAnswers = function() {
   $(aOne).on('click', function(e) {
     if (aOne.text() === answers[i]) {
@@ -153,6 +168,8 @@ const checkAnswers = function() {
     }
   });
 }
+
+// code to execute on a correct answer selection
 const rightAnswer = function() {
   $('#failure').addClass("hidden");
   $('#hellothere').addClass("hidden");
@@ -162,6 +179,7 @@ const rightAnswer = function() {
   }
 }
 
+// code to execute on an incorrect answer selection
 const wrongAnswer = function() {
   $('#hellothere').addClass("hidden");
   $('#yodadance').addClass("hidden");
@@ -169,6 +187,8 @@ const wrongAnswer = function() {
   new Audio("audio/muchfear.mp3").play();
 }
 
+
+// code to execute on game start "Begin Your Training" button click
 $('#startGame').on('click', function(e) {
   $('#trainingJedi').hide();
   $('#masterJedi').hide();
